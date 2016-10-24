54a00ebc0afc4db83307e5a951fc030672ba7fd0
bverjat <qwsx@dcfvg.com>
2016-10-24 16:09:52 +0200

    working on card list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import params from 'query-params';
 
 const Card = ({
-  name
-}) => (
-  <div>{name}</div>
-)
+  name,
+  url,
+  image,
+  source,
+  description_en
+}) => {
+  const manetInstance = 'http://manet.medialab.sciences-po.fr/?';
+  const captureParams = {
+    url:url,
+    delay:5000,
+    format:'jpeg',
+    width:300,
+    height:170,
+    zoom:0.25,
+    clipRect:'0,0,300,170'
+  };
+  const capture = manetInstance + params.encode(captureParams);
+
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <img src={ image ? image : capture }/>
+      {source ? <a target="_blank" href={source}>source</a> : ''}
+      {url ? <a target="_blank" href={url}>try it !</a> : ''}
+      <p>{description_en}</p>
+
+    </div>
+  )
+}
 
 export default Card;
